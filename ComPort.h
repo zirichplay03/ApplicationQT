73fedc7ea22590138a93e64339431054b8aa16b5
Kirill Kuznetsov <k34kirill0@gmail.com>
2026-02-03 09:23:13 +0000

    .

---
+++ ComPort.h
@@ -0,0 +1,33 @@
+#ifndef COMPORTBASE_H
+#define COMPORTBASE_H
+
+#include <unistd.h>
+#include <fcntl.h>
+#include <termios.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdint.h>
+#include <dirent.h>
+#include <string>
+#include <vector>
+
+#define PORT_PREFIX "/dev/"
+
+class ComPortBase {
+public:
+    int setup_serial_port();
+    int search_port();
+    const char* port;
+    unsigned int speed_t;
+private:
+    std::string name;
+    DIR* dir;
+    struct dirent* entry;
+    char full_port[50];
+    int serial_port;
+    struct termios tty;
+    int found_ports = 0;
+    int connected_port = -1;
+};
+#endif
